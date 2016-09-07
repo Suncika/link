12832aad1da22419d22c107573bf59f78025ed82
animejahe <mirror.anime.id@gmail.com>
2016-09-07 07:13:59 +0700

    Menambah fitur minify json

---
+++ index.js
@@ -0,0 +1,25 @@
+var fs = require('fs');
+
+var folder = 'raw/';
+
+fs.readFile(folder+'index.json', 'utf8', function(err, data) {
+    var json = JSON.parse(data);
+
+    fs.writeFile('index.json', JSON.stringify(json), function(err) {
+        if (err) throw err;
+        console.log('index.json tersimpan!');
+    });
+});
+
+fs.readdir(folder+'anime', function(err, files) {
+    files.forEach(function(file) {
+        fs.readFile(folder+'anime/'+file, 'utf8', function(err, data) {
+            var json = JSON.parse(data);
+
+            fs.writeFile('anime/'+file, JSON.stringify(json), function(err) {
+                if (err) throw err;
+                console.log(file+' tersimpan!');
+            });
+        });
+    });
+});
